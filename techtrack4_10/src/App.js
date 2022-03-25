9e60c90eda03368cc0856a327a2cfcd92545c663
Xiang Kai <xkleow.2018@smu.edu.sg>
2022-03-25 13:56:12 +0800

    Re-done log in interface without template

--- techtrack4_10/src/App.js
+++ techtrack4_10/src/App.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import Login from "./components/Login.js";
+import Navbar from "./components/Navbar.js";
 
 export default function App() {
+  return(
   <div>
+    <Navbar />
     <Login />
-  </div>;
+  </div>
+  )
 }
---
+++ techtrack4_10/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav >
+            <h2 >DBS</h2>
+            <h4 >Log In</h4>
+            <h4 >Register</h4>
+        </nav>
+    )
+}
